f999227fc486adbc3c1de146698acc1de6f23d19
Simmi <simmisingh217@gmail.com>
2021-07-13 10:44:38 +0530

    Added changes in Branch A1

--- src/package2/Class2.java
+++ src/package2/Class2.java
@@ -3,6 +3,7 @@ package package2;
 public class Class2 {
 	public static void main(String[] args) {
 		System.out.println("2");
-		System.out.println("2.1");
+		System.out.println("2.1 in Branch A1");
+		System.out.println("I am in Branch A1");
 	}
 }
