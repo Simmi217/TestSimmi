1a6d8c408aa974a4aea4cb441d628748f1e17a89
Simmi <simmisingh217@gmail.com>
2021-07-13 10:48:00 +0530

    Added Branch B1 changes

--- src/package2/Class2.java
+++ src/package2/Class2.java
@@ -3,6 +3,7 @@ package package2;
 public class Class2 {
 	public static void main(String[] args) {
 		System.out.println("2");
-		System.out.println("2.1");
+		System.out.println("2.1 In Branch B1");
+		System.out.println("Checking conflicts scenerios");
 	}
 }
